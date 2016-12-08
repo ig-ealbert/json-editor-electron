dd05839a02f067aa6b1703ef3e23239b4f4e6f48
ig-ealbert <ealbert@infragistics.com>
2016-12-08 12:41:28 -0500

    Initial commit

---
+++ buttons.js
@@ -0,0 +1,56 @@
+﻿const dialog = require('electron').remote.dialog; // access file system dialogs
+
+// Show an open file dialog, select a file, and load it into the editor
+function openFile(){
+  dialog.showOpenDialog(
+    {filters: [{ name: 'json', extensions: ['json'] }]},
+	function (fileName) {
+    if (fileName !== undefined)
+	{
+	  readFile(fileName[0]);
+	  openFileName = fileName[0];
+	}
+  });
+}
+
+// Open the new file template and load it into the editor
+function newFile(){
+  readFile("newFileTemplate.json");
+  openFileName = "";
+}
+
+// Helper function for saving a file
+function writeFile(fileName){
+  fs.writeFile(fileName, data, function (err) {
+    if(err){
+      alert("An error occurred creating the file " + err.message);
+	}
+  });
+}
+
+// Save the content of the editor into a JSON file
+function saveFile() {
+  parsedJson = editor.get();
+  data = JSON.stringify(parsedJson, null, 2);
+  
+  if (openFileName == "")
+  {
+	dialog.showSaveDialog(
+	  {filters: [{ name: 'json', extensions: ['json'] }]},
+	  function (fileName) {
+      if (fileName !== undefined)
+	  {
+	    writeFile(fileName);
+	  }
+    });
+  }
+  else
+  {
+    writeFile(openFileName);
+  }
+}
+
+// Add the event handlers to the buttons
+document.getElementById('btnOpenFile').addEventListener('click', openFile);
+document.getElementById('btnNewFile').addEventListener('click', newFile);
+document.getElementById('btnSaveFile').addEventListener('click', saveFile);
---
+++ initialization.js
@@ -0,0 +1,20 @@
+﻿const fs = require('fs'); // fs module allows for file system operations
+var openFileName = "";
+
+// Create a JSONEditor widget in the "jsonEditor" div
+var editor = new JSONEditor(document.getElementById("jsonEditor"), {});
+var parsedJson;
+
+// Use the fs (file system) library to read a file and populate the editor
+function readFile(filepath){
+  fs.readFile(filepath, 'utf-8', function (err, data) {
+    if (err){
+	  alert("An error occurred reading the file: " + err.message);
+	  return;
+	}
+	parsedJson = JSON.parse(data);
+	editor.set(parsedJson);
+  });
+}
+
+var data = readFile("newFileTemplate.json"); // Set an initial "blank" file
